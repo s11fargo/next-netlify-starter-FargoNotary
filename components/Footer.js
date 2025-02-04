9b2d6f5fbf3efc622b43b50ca2e32573e629e5a4
s11fargo <122173639+s11fargo@users.noreply.github.com>
2025-02-04 13:58:13 -0800

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -3,9 +3,13 @@ import styles from './Footer.module.css'
 export default function Footer() {
   return (
     <>
-      <footer className={styles.footer}>
-        <img src="/logo-netlify.svg" alt="Netlify Logo" className={styles.logo} />
-      </footer>
+    <footer className={styles.footer}>
+        <ul class="mylinks">
+             <li><a href="https://www.signingagent.com/profile/161876044//" target="_blank">Signing Agent Page</a></li>
+             <li><a href="https://www.yelp.com/" target="_blank">Yelp</a></li>
+             <li><a href="https://fargosoftware.netlify.app/" target="_blank">Made by Fargo Software</a></li>
+        </ul>      
+    </footer>
     </>
   )
 }

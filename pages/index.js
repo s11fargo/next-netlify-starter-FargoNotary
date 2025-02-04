321ebd4cee9df389c58e6b71e76b1f289a1c0d20
s11fargo <122173639+s11fargo@users.noreply.github.com>
2025-02-04 14:13:59 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,19 +6,19 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Fargo Software</title>
+        <title>Fargo Notary</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <Header title="Fargo Notary" />
         <p className="details">
-          For Notary Needs Text (818) 370-4234 or Email: FargoNotary@yahoo.com 
+          For Notary Needs: Text (818) 370-4234 or Email FargoNotary@yahoo.com 
         </p>
       </main>
   
       <body>
-        <img src="Software.jpg" width="600" height="400"></img>
+        <img src="nsa_member_badge.png" width="688" height="758"></img>
       </body>
 
       <Footer />

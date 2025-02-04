f76df4292c2655022426d6044f176783fb3664f6
s11fargo <122173639+s11fargo@users.noreply.github.com>
2025-02-04 14:52:10 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
     <div className="container">
       <Head>
         <title>Fargo Notary</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/StampIcon.png" />
       </Head>
 
       <main>

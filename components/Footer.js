4a6ac68aab7cb9a0b527083a41c2c800b8ee4934
s11fargo <122173639+s11fargo@users.noreply.github.com>
2025-02-04 14:35:40 -0800

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -5,9 +5,9 @@ export default function Footer() {
     <>
     <footer className={styles.footer}>
         <ul class="mylinks">
-             <li><a href="https://www.signingagent.com/profile/161876044/" target="_blank">Signing Agent Page</a></li>
-             <li><a href="https://www.yelp.com/" target="_blank">Yelp</a></li>
-             <li><a href="https://fargosoftware.netlify.app/" target="_blank">Made by Fargo Software</a></li>
+             <li><a href="https://www.signingagent.com/profile/161876044" target="_blank">Signing Agent Page</a></li>
+             <li><a href="https://www.yelp.com" target="_blank">Yelp</a></li>
+             <li><a href="https://fargosoftware.netlify.app" target="_blank">Made by Fargo Software</a></li>
         </ul>      
     </footer>
     </>

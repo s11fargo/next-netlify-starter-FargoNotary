31804bfc3b65ec1fa37a702437ba819861a5ab36
s11fargo <122173639+s11fargo@users.noreply.github.com>
2025-02-05 09:20:18 -0800

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -6,8 +6,8 @@ export default function Footer() {
     <footer className={styles.footer}>
         <ul class="mylinks">
              <li><a href="https://www.signingagent.com/profile/161876044" target="_blank">Signing Agent Profile</a></li>
-             <li><a href="https://www.linkedin.com/in/sharon-fargo-a71b1054" target="_blank">LinkedIn</a></li>
              <li><a href="https://www.notaryresume.com/000761148E256A44F758C621737462198522" target="_blank">Notary Resume</a></li>
+             <li><a href="https://www.linkedin.com/in/sharon-fargo-a71b1054" target="_blank">LinkedIn</a></li>
              <li><a href="https://www.yelp.com" target="_blank">Yelp</a></li>
              <li><a href="https://fargosoftware.netlify.app" target="_blank">Made by Fargo Software</a></li>
         </ul>      
